d16b6d1d1937c426644eb162e232e6e4d12066ef
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-07-23 11:48:00 -0300

    Toggle diffs

--- content.js
+++ content.js
@@ -1,12 +1,56 @@
 ;(function () {
   'use strict'
 
-  // -----------------------------------------------------------------------------
-  // Show outdated diffs by default
+  var urlObserver = new window.MutationObserver(function(mutations, observer) {
+    setTimeout(main)
+  })
 
-  let outdatedDiffs = document.querySelectorAll('.outdated-diff-comment-container')
-  
-  for(let i = 0; i < outdatedDiffs.length; i++) {
-    outdatedDiffs[i].classList.add('open')
+  urlObserver.observe(document.getElementById('js-pjax-loader-bar'), {
+    attributes: true,
+    attributeFilter: ['class']
+  })
+
+  main()
+
+  function main() {
+    // -----------------------------------------------------------------------------
+    // Show outdated diffs by default
+
+    let outdatedDiffs = document.querySelectorAll('.outdated-diff-comment-container')
+
+    for(let i = 0; i < outdatedDiffs.length; i++) {
+      outdatedDiffs[i].classList.add('open')
+    }
+
+    
+    // -----------------------------------------------------------------------------
+    // Collapse diffs
+
+    let headers = document.querySelectorAll('.file-header')
+
+    for(let i = 0; i < headers.length; i++) {
+      headers[i].addEventListener('click', togglePanel)
+      headers[i].style.cursor = 'pointer'
+    }
+
+    function togglePanel() {
+      let code = nextByClass(this, 'blob-wrapper')
+      if (code) {
+        code.classList.toggle('hidden')
+      }
+    }
   }
+
+
+  // -----------------------------------------------------------------------------
+  // Utils
+
+  function nextByClass(node, className) {
+    while (node = node.nextSibling) {
+      if (node.classList && node.classList.contains(className)) {
+        return node
+      }
+    }
+  }
+
 })()
