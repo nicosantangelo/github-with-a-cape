190e7b4d0b96ef34d4da4a03d3b4db2ea663d655
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-07-24 22:50:41 -0300

    Open on install

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(function(details) {
-    if(details.reason == 'update') {
+    if(details.reason === 'install' || details.reason === 'update') {
       var OPTIONS_URL = chrome.extension.getURL('options/options.html')
       chrome.storage.local.set({ justUpdated: true }, function () {
         chrome.tabs.create({ url: OPTIONS_URL })
