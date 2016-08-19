e85affd59668a093c94e72b6c621ed6ff5f9d5a3
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-08-19 02:42:07 -0300

    Load notifications on each click, adding 'If-Modified-Since' accordingly

--- notifications.js
+++ notifications.js
@@ -2,37 +2,80 @@
 
 (function() {
   var NOTIFICATIONS_HTML = chrome.extension.getURL('notifications.html')
+  var API_URL = null
+  var lastRequest = {
+    lastModified: '',
+    text: ''
+  }
+
+  getApiURL(function(url) { API_URL = url })
 
   window.notifications = {
     start: function() {
-      getApiURL(renderNotifications)
+      http({ method: 'GET', url: NOTIFICATIONS_HTML }, function(modalHTML) {
+        appendModal(modalHTML)
+
+        var indicator = document.querySelector("#user-links .notification-indicator")
+        indicator.classList.remove('tooltipped-s')
+        indicator.classList.add('tooltipped-w')
+
+        var modal = document.getElementById('__ghcape-modal')
+        modal.style.left = (indicator.offsetLeft + indicator.offsetWidth - 300) + "px"
+
+        indicator.addEventListener('click', function(event) {
+          if (event.which === 2) return // 'Middle' mouse click
+
+          event.preventDefault()
+          indicator.blur()
+
+          if (modal.classList.contains('hidden')) {
+            modal.classList.remove('hidden')
+            document.body.addEventListener('click', hideNotificationsModalOnOutsideClick)
+            requestNotifications()
+          } else {
+            modal.classList.add('hidden')
+            document.body.removeEventListener('click', hideNotificationsModalOnOutsideClick)
+          }
+        }, true)
+
+        document.getElementById('__ghcape-mark-as-read').addEventListener('click', readNotifications, true)
+      })
     }
   }
 
-  function renderNotifications(url) {
-    http({ method: 'GET', url: NOTIFICATIONS_HTML }, function(modalHTML) {
-      var div = document.createElement('div')
-      div.innerHTML = modalHTML
-      document.body.appendChild(div)
+  function appendModal(modalHTML) {
+    var modalContainer = document.createElement('div')
+    modalContainer.innerHTML = modalHTML
+    document.body.appendChild(modalContainer)
+  }
 
-      http({ method: 'GET', url: url }, appendNotifications)
+  function requestNotifications() {
+    setEmptyNotificationsNotice('Loading...')
+
+    http({
+      method: 'GET',
+      url: API_URL,
+      headers: { 'If-Modified-Since': lastRequest.lastModified }
+    }, function(responseText, fullResponse) {
+      lastRequest.lastModified = fullResponse.getResponseHeader('Last-Modified') || lastRequest.lastModified
+      
+      var text = responseText === ''
+        ? lastRequest.text
+        : responseText
+
+      renderNotifications(text, fullResponse)
+      lastRequest.text = text
     })
   }
 
-  function appendNotifications(notificationsResponse, response) {
-    var linkheader = response.getResponseHeader('Link')
-
-    var indicator = document.querySelector("#user-links .notification-indicator")
-    indicator.classList.remove('tooltipped-s')
-    indicator.classList.add('tooltipped-w')
-
-    var modal = document.getElementById('__ghcape-modal')
-    modal.style.left = (indicator.offsetLeft + indicator.offsetWidth - 300) + "px"
-
-    var notifications = JSON.parse(notificationsResponse)
+  function renderNotifications(notificationsText, fullResponse) {
+    var linkheader = fullResponse.getResponseHeader('Link')
+    var notifications = JSON.parse(notificationsText)
     var notificationsList = ''
 
     if (notifications.length > 0) {
+      var modal = document.getElementById('__ghcape-modal')
+
       var iconsHTML = {
         pull   : modal.querySelector('.octicon-git-pull-request').outerHTML,
         issues : modal.querySelector('.octicon-issue-opened').outerHTML,
@@ -49,38 +92,19 @@
     } else {
       setEmptyNotificationsNotice()
     }
-
-
-    document.getElementById('__ghcape-mark-as-read').addEventListener('click', readNotifications, true)
-
-    indicator.addEventListener('click', function(event) {
-      // 'Middle' mouse click
-      if (event.which === 2) return
-
-      event.preventDefault()
-      
-      indicator.blur()
-
-      if (modal.classList.contains('hidden')) {
-        modal.classList.remove('hidden')
-        document.body.addEventListener('click', hideNotificationsModalOnOutsideClick)
-      } else {
-        modal.classList.add('hidden')
-        document.body.removeEventListener('click', hideNotificationsModalOnOutsideClick)
-      }
-    }, true)
   }
 
   function buildNotificationsListHTML(notifications, iconsHTML) {
     return notifications.map(function(notification) {
-      let subject = notification.subject
+      var subject = notification.subject
 
-      let resourceId = subject.url.split('/').slice(-1)
-      let type = { Issue: 'issues', PullRequest: 'pull', Commit: 'commits' }[subject.type]
+      var resourceId = subject.url.split('/').slice(-1)
+      var type = { Issue: 'issues', PullRequest: 'pull', Commit: 'commits' }[subject.type]
 
-      let url = notification.repository.html_url + '/' + type + '/' + resourceId
+      var url = notification.repository.html_url + '/' + type + '/' + resourceId
+      var title = escapeHTML(subject.title)
 
-      let link = '<a href="' + url + '" data-id="' + notification.id + '">' + iconsHTML[type] + subject.title + '</a>'
+      var link = '<a href="' + url + '" data-id="' + notification.id + '">' + iconsHTML[type] + title + '</a>'
 
       return '<li>' + link + '</li>'
     })
@@ -92,19 +116,18 @@
         method: 'PUT',
         url: url,
         data: { read: true }
-      }, setEmptyNotificationsNotice)
+      }, setEmptyNotificationsNotice.bind('No new notifications'))
     })
   }
 
   function hideNotificationsModalOnOutsideClick(event) {
     // Take the less reliable but more efficient route
-    var targetClassList = event.target.classList
-    var parentClassList = event.target.parentElement.classList
-    var clickedOnNotifications = targetClassList.contains('notification-indicator') || parentClassList.contains('octicon-bell')
-
-    var modal = document.getElementById('__ghcape-modal')
+    var classes = event.target.classList + " " + event.target.parentElement.classList
+    var notificationClasses = /notification-indicator|octicon-bell/
+    var clickedOnNotifications = classes.search(notificationClasses) !== -1
 
     if (! clickedOnNotifications) {
+      var modal = document.getElementById('__ghcape-modal')
       modal.classList.add('hidden')
     }
   }
@@ -117,7 +140,7 @@
 
     xmlhttp.onreadystatechange = function(result) {
       if (xmlhttp.readyState === 4) {
-        if(xmlhttp.status >= 300) {
+        if(xmlhttp.status >= 400) {
           error && error(xmlhttp.responseText, xmlhttp)
         } else {
           success(xmlhttp.responseText, xmlhttp)
@@ -127,6 +150,13 @@
     }
 
     xmlhttp.open(options.method, options.url, true)
+
+    if (options.headers) {
+      for(var header in options.headers) {
+        xmlhttp.setRequestHeader(header, options.headers[header])
+      }
+    }
+
     xmlhttp.send(JSON.stringify(options.data))
   }
 
@@ -138,8 +168,12 @@
     })
   }
 
-  function setEmptyNotificationsNotice() {
-    document.getElementById('__ghcape-notifications-list').innerHTML = '<li class="__ghcape-empty">No new notifications</li>'
+  function setEmptyNotificationsNotice(text) {
+    document.getElementById('__ghcape-notifications-list').innerHTML = '<li class="__ghcape-empty">' + text + '</li>'
+  }
+
+  function escapeHTML(html) {
+    return html.replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&apos;').replace(/>/g, '&gt;').replace(/</g, '&lt;')
   }
 
 })()
